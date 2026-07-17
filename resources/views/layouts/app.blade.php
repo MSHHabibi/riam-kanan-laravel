3f7af74cd2f4330a55e55f9718487f903f0b2cee
Muhammad Satrio Habibi• <138227369+MSHHabibi@users.noreply.github.com>
2026-07-18 02:14:38 +0700

    Fix CSS loading path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="/css/style.css">
 
     <link
         rel="stylesheet"
