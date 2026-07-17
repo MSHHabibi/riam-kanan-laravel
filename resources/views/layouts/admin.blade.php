17b76a53c5f492e8e1346a4e342c4ced12c02dc4
Muhammad Satrio Habibi• <138227369+MSHHabibi@users.noreply.github.com>
2026-07-18 02:53:39 +0700

    D

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin - Riam Kanan Explorer</title>
 
-    <link rel="stylesheet" href="/css/style.css?v=123">
+    <link rel="stylesheet" href="/css/style.css?v=999">
 </head>
 <body>
     <div class="layout">
